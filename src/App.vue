183b68f6f6838a5ee032ae348b2dd8d6aedd44d1
Joshua Gulledge <jgulledge@ddmsinc.com>
2022-01-20 08:35:20 -0600

    using ajax to get data from api source

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
 
+    <button v-on:click="buttonClick">
+      Load Data
+    </button>
    <!-- after we import syncfusion we can use the ejs-grid tag -->
-   <ejs-grid 
-    :dataSource="data">
-
-     <!-- to customize the columns -->
+   <ejs-grid ref="dataGrid">
+     <!-- using a ref to get the data returned from ajax -->
      <e-columns>
        <!-- include the column sub tab -->
-
        <e-column 
         field="OrderID"
         headerText="Order Id" 
         textAlign="Right">
        </e-column>
-
+        <!-- field = Variable Name / headerText = Displayed Name -->
        <e-column 
         field="CustomerID"
         headerText="Customer Id" 
@@ -47,18 +47,30 @@
 // imports
 import Vue from 'vue';
 import { GridPlugin } from '@syncfusion/ej2-vue-grids';
-import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
-
+import { Ajax } from '@syncfusion/ej2-base';
 
 Vue.use(GridPlugin);
+
 export default {
   // temp hardcode some data to be displayed in the grid
   data () {
     return {
-     data: new DataManager({
-       url: "https://ej2services.syncfusion.com/production/web-services/api/Orders?$top=7",
-       adaptor: new WebApiAdaptor()
-     })
+     
+    }
+  },
+  methods: {
+    buttonClick: function() {
+      // define the ref variable for the grid 
+      let grid = this.$refs.dataGrid;
+      const ajax = new Ajax(
+        "https://ej2services.syncfusion.com/production/web-services/api/Orders",
+        "GET"
+      );
+      ajax.send();
+      ajax.onSuccess = function(results) {
+        // set the ref variable with returned data
+        grid.ej2Instances.setProperties({dataSource: JSON.parse(results)});
+      }
     }
   }
 }
